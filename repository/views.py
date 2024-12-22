c70fe34447f102693fb3156004cbe13fbdca798d
QuintonStepps <qstepps@gmail.com>
2024-12-21 20:23:21 -0600

    added seraching full implemented

--- repository/views.py
+++ repository/views.py
@@ -40,8 +40,42 @@ def upload(request):
 
 def index(request):
     documents = Document.objects.all()
+    if(request.method == 'POST'):
+        
+        return search(request)
     
 
     return render(request, 'repository/index.html', {'documents': documents})
 
+def search(request):
+    documents = Document.objects.all()
+    if request.method == 'POST':
+        city = request.POST['city']
+        country = request.POST['country']
+        major = request.POST['major']
+        author = request.POST['author']
+        advisor = request.POST['advisor']
+        themes = request.POST['themes']
+        year = request.POST['year']
+        if(year != ''):
+            documents = documents.filter(year=year)
+        if(author != ''):
+            documents = documents.filter(author=author)
+        if(advisor != ''):
+            documents = documents.filter(advisor=advisor)    
+        if(city != ''):
+            documents = documents.filter(city=city)
+        if(country != ''):
+            documents = documents.filter(country=country)
+        if(major != ''):
+            documents = documents.filter(major=major)
+        if(themes != ''):
+            documents = documents.filter(themes=themes)
+        
 
+        
+        
+        return render(request, 'repository/index.html', {'documents': documents})
+    else:
+        ()
+        return render(request, 'repository/index.html', {'documents': documents})
